ac82a16addc7a7023c237e9ba8292d7ae7646115
Aman2205saini <137211234+Aman2205saini@users.noreply.github.com>
2024-11-25 13:51:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // your code here
+     document.querySelector('form').addEventListener('submit', function(event) {
+            event.preventDefault();
+            var name = document.querySelector('[name="name"]').value;
+            var year = document.querySelector('[name="year"]').value;
+            if(name !="" && year !="")
+            document.getElementById('change').innerHTML = "http://localhost:8080/?name=" + name + "&year=" + year;
+            else if(name =="" && year !="")
+            document.getElementById('change').innerHTML = "http://localhost:8080/?year=" + year ;
+            else if(name !="" && year =="")
+            document.getElementById('change').innerHTML = "http://localhost:8080/?name=" + name ;
+            else
+            document.getElementById('change').innerHTML = "http://localhost:8080/" ;
